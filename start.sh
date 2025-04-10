09f7b586e8cefeec02a6474946de68c899db3084
AbnerCerqueira <abner.cerqueira@fatec.sp.gov.br>
2025-04-09 21:42:07 -0300

    commit inicial

---
+++ start.sh
@@ -0,0 +1,6 @@
+
+yay -S --needed noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono ttf-jetbrains-mono-nerd kitty hyprland xdg-desktop-portal-hyprland hypridle hyprlock hyprpaper hyprshot swaync cliphist nwg-look nautilus qt5ct qt6ct pavucontrol blueman-manager waybar fastfetch rofi openrbg stow
+
+mv ~/.config/hypr ~/.config/hypr_bak
+
+stow .
